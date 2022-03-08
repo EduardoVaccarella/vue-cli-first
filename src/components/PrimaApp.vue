797f18917dd8361bdea13a6c0e7806c43bb904b8
eduardoego <eduardo0vaccarella@gmail.com>
2022-03-08 20:59:13 +0100

    Finto

---
+++ src/components/PrimaApp.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>
+      La mia prima App con Vue CLI!
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+div {
+  display: flex;
+  justify-content: center;
+
+  h1 {
+    color: grey;
+  }
+}
+</style>
